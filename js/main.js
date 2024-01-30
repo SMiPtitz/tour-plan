76627b4f7e066a41ccbe0e70ecd682bc637421b0
SMiPtitz <ptitzalone@gmail.com>
2024-01-30 19:06:22 +0300

    Create: add modal window

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-const hoterSlider = new Swiper('.hotel-slider', {
+$(document).ready(function() {
+
+  const hoterSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
 
@@ -7,19 +9,43 @@ const hoterSlider = new Swiper('.hotel-slider', {
     nextEl: '.hotel-slider__button--next',
     prevEl: '.hotel-slider__button--prev',
   },
-});
-const reviewsSlider = new Swiper('.reviews-slider', {
-  // Optional parameters
-  loop: true,
+  });
+  const reviewsSlider = new Swiper('.reviews-slider', {
+    // Optional parameters
+    loop: true,
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
-  },
-});
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
+    },
+  });
+  
+// true JS
+  var menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener('click', function () {
+    document.querySelector('.navbar-bottom').classList.toggle('navbar-bottom--visible');
+  });
 
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener('click', function () {
-  document.querySelector('.navbar-bottom').classList.toggle('navbar-bottom--visible');
+// JQuery
+  var modalButton = $('[data-toggle="modal"]');
+  var closeModalButton = $(".modal__close");  
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay--active");
+    modalDialog.addClass("modal__dialog--active");
+  }
+
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--active");
+    modalDialog.removeClass("modal__dialog--active");
+  }
+
 });
